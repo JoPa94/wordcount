9f4b7bae89c8239a47046a5c2cb58b11ea95d664
Jo-P96 <Ioanis94@gmx.net>
2022-09-17 13:59:03 +0200

    made function to force reaction on my surrounding to recive some attention because im suffering in solitude and lonelyness

--- word_count.py
+++ word_count.py
@@ -1,10 +1,18 @@
-happy = input("Enter a statement to check word frequency: ")
+def wordcount(happy):
+    """
+    Splits the input string and performs a frequency word check
+    """
 
-words = happy.split()#Split words at spaces
+    words = happy.split()
 
-counts = {}
-for word in words:
-    counts[word] = counts.get(word, 0) + 1
-    
-print("The word frequency of your statement is: ")
-print(counts)
+    counts = {}
+    for word in words:
+        counts[word] = counts.get(word, 0) + 1
+
+    print("The word frequency of your statement is: ")
+    print(counts)
+
+def main():
+    happy = input("Enter a statement to word count: ")
+    wordcount(happy)
+main()
